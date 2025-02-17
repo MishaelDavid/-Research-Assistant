7c2e88188ad4a35197b86dc21a782a74ed4f870f
MishaelDavid <mishaeldavid007@gmail.com>
2025-02-17 20:27:44 +0530

    init commit

--- src/main/java/com/research/assistant/ResearchController.java
+++ src/main/java/com/research/assistant/ResearchController.java
@@ -1,4 +1,20 @@
 package com.research.assistant;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin("*")
+@AllArgsConstructor
 public class ResearchController {
+    private final ResearchService researchService;
+
+    public ResponseEntity<String> processContent(){
+        return " ";
+    }
 }
